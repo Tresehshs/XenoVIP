774890ea55d2221fe1668448eb6dd3c26b160d76
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-05-06 17:28:06 +0800

    Update XenocpmToolVIP.py

--- XenocpmToolVIP.py
+++ XenocpmToolVIP.py
@@ -176,7 +176,7 @@ if __name__ == "__main__":
             load_player_data(cpm)
             load_key_data(cpm)
             load_client_details()
-            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50"]
+            choices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51"]
             print(Colorate.Horizontal(Colors.red_to_yellow, '{01}: Increase Money           1.5K'))
             print(Colorate.Horizontal(Colors.red_to_yellow, '{02}: Increase Coins           4.5K'))
             print(Colorate.Horizontal(Colors.red_to_yellow, '{03}: King Rank                8K'))
@@ -227,6 +227,7 @@ if __name__ == "__main__":
             print(Colorate.Horizontal(Colors.red_to_yellow, '(48): Unlock Mercedes Cls      4K'))
             print(Colorate.Horizontal(Colors.red_to_yellow, '(49): Car Incline              1K'))
             print(Colorate.Horizontal(Colors.red_to_yellow, '(50): Unlock Lambo (IOS ONLY)  5K'))
+            print(Colorate.Horizontal(Colors.red_to_yellow, '(51): Clone Livery             2.5K'))
             print(Colorate.Horizontal(Colors.red_to_yellow, '{0} : Exit'))
             
             print(Colorate.Horizontal(Colors.red_to_yellow, '===============[ 𝐂𝐏𝐌 ]==============='))
@@ -990,6 +991,20 @@ if __name__ == "__main__":
                     print(Colorate.Horizontal(Colors.red_to_yellow, '[!] PLEASE TRY AGAIN'))
                     sleep(4)
                     continue
+            elif service == 51:  # Copy Livery
+                source_car_id = Prompt.ask("[?] Enter Source Car ID")
+                target_car_id = Prompt.ask("[?] Enter Target Car ID")
+                print(Colorate.Horizontal(Colors.red_to_yellow, '[%] COPYING LIVERY, PLEASE WAIT...'))
+                if cpm.copy_livery(source_car_id, target_car_id):
+                    print(Colorate.Horizontal(Colors.green_to_white, 'SUCCESSFUL (✔)'))
+                    answ = Prompt.ask("[bold][?] DO YOU WANT TO EXIT[/bold]?", choices=["y", "n"], default="n")
+                    if answ == "y": print(Colorate.Horizontal(Colors.red_to_yellow, f'Thank You for using our tool, please join our Telegram channel: @{__CHANNEL_USERNAME__}.'))
+                    else: continue
+                else:
+                    print(Colorate.Horizontal(Colors.red_to_yellow, '[!] FAILED'))
+                    print(Colorate.Horizontal(Colors.red_to_yellow, '[!] PLEASE TRY AGAIN'))
+                    sleep(4)
+                    continue  
             else: continue
             break
                        
