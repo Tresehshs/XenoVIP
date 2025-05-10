cd1f52c73953730a5ce342f4a5fb0d2b0bf65a0a
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-05-10 20:01:50 +0800

    Update xenocpmvip.py

--- xenocpmvip.py
+++ xenocpmvip.py
@@ -467,7 +467,6 @@ class Tresehshs:
         params = { "key": self.access_key, "new_hp": new_hp, "new_inner_hp": new_inner_hp, "new_nm": new_nm, "new_torque": new_torque }
         response = requests.post(f"{__ENDPOINT_URL__}/speed_all_cars", params=params, data=payload)
         response_decoded = response.json()
-        self.log_action("speed_all_cars", { "payload": payload, "params": params })
         return response_decoded.get("ok")
  
 
