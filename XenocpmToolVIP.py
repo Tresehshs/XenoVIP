fbbea2c5109ad9ead59513fe68f39a3228f42f3d
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-05-19 02:31:13 +0800

    Update XenocpmToolVIP.py

--- XenocpmToolVIP.py
+++ XenocpmToolVIP.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
         acc_email = prompt_valid_value("[bold][?] Account Email[/bold]", "Email", password=False)
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
-        console.print("[bold cyan][%] Trying to Login[/bold white]: ", end=None)
+        console.print("[bold white][%] Trying to Login[/bold white]: ", end=None)
         cpm = Tresehshs(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
